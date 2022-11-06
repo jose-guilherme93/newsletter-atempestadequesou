5fe0aefe5fb3aacef896d9cfbcef7f6a7f0e2f28
jose-guilherme93 <jose-guilherme93@hotmail.com>
2022-11-05 23:53:16 -0300

    first layout

--- frontend/pages/components/Form.tsx
+++ frontend/pages/components/Form.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image"
 import { useForm } from "react-hook-form"
 
 export default function Form() {
@@ -6,23 +6,42 @@ export default function Form() {
     const onSubmit = (data: any) => console.log(data)
 
     return (
-        <form className="w-screen h-screen flex flex-col items-center justify-center bg-blue-500 m-auto" onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input 
-            className=" p-4 m-4 w-[400px] " 
-            defaultValue="test"
-            placeholder="Seu nome"
-            {...register("example")} />
+       <div className="h-full w-full">
+        <Image
+        priority
+        src="/logo.jpg" 
+        width={120} 
+        height={120} 
+        alt="logo"
+        className="relative top-36 m-auto rounded-full"/>
+         <form className=" w-full h-full flex flex-col items-center justify-center" onSubmit={handleSubmit(onSubmit)}>
+            
+            <label className="flex w-full flex-col justify-center items-center" htmlFor="inputName">
+                <span className="text-left w-96 text-white font-mono font-bold mb-2">seu nome mais bonito</span>
+                <input
+                id="inputName"
+                className=" w-96 h-12 focus border-2 border-transparent  focus:border-yellow-400 hover:border-yellow-400 rounded-3xl mb-4 font-bold text-zinc-500" 
+                type="text"
+                placeholder="seu nome"
+                {...register("example", { required: true })}  />
+                {errors.exampleRequired && <span className="text-white font-mono font-bold">ei, não esqueça seu nome</span>}
+            </label>
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input 
-            className="border-black p-4 m-4 w-2/5 "
-            placeholder="Seu Email"
-            {...register("exampleRequired", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+            <label htmlFor="inputEmail" className="flex flex-col w-full justify-center items-center">
+                <span className="text-left w-96 text-white font-mono font-bold mb-2 ">digite seu email</span>
+                <input
+                id="inputEmail"
+                type="email"
+                className="w-96 h-12 border-transparent  focus:border-yellow-400 border-2 hover:border-yellow-400 rounded-3xl font-bold text-zinc-500"
+                placeholder="moranguinho23@exemplo.com"
+                {...register("exampleRequired", { required: true })} />
+                {/* errors will return when field validation fails  */}
+                {errors.exampleRequired && <span className="text-white font-mono font-bold">esse email está meio estranho...</span>}
+            </label>
+                
 
-            <input className="border-black p-4 m-4 w-2/5 " type="submit" />
+            <button className="  text-white border-solid border-2  rounded-3xl w-48 mt-4 hover:bg-yellow-300  hover:border-yellow-300" type="submit">Enviar </button>
         </form>
+       </div>
     )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,14 @@
-
-
-import { ApiError } from "next/dist/server/api-utils"
 import Form from "./components/Form"
-
 
 export default function Home() {
  
   
   return (
-    <Form />
+    <main className="flex items-center justify-center w-full h-screen bg-blue-500">
+      <div className="w-1/2 text-center font-bold font-mono text-white ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tenetur adipisci quis quod, architecto quasi itaque vero dolore similique eius corporis, pariatur perferendis quas eaque cupiditate repellendus consequatur hic voluptatibus?
+      </div>
+      <Form />
+    </main>
   )
 }
