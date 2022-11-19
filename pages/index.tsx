0168bf91b21ad48753ca319722ef0aa1e249bf03
jose-guilherme93 <jose-guilherme93@hotmail.com>
2022-11-18 23:34:16 -0300

    added title to the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>This page has a title 🤔</title>
+        <title>A tempestade que sou</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Página de captura de email para eventuais atualizações do instagram @atempestadequesou "/>
         <meta name="keywords" content="newsletter, landing page"/>
         <meta name="author" content="A tempestade que sou"/>
       </Head>
-      
+
     <main className=" w-full h-full sm:block md:flex bg-gradient-to-l from-yellow-300 to-blue-600 ">
 
       <Form />
