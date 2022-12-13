909099055291b9d7c31052a21eadcfed9fe88dae
jose-guilherme93 <jose-guilherme93@hotmail.com>
2022-12-12 21:27:22 -0300

    feature: new email text to subscribers

--- pages/api/server/subscribe.ts
+++ pages/api/server/subscribe.ts
@@ -1,5 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 import nodemailer from 'nodemailer'
+
 import {MongoClient} from 'mongodb'
 
 async function connectToDatabase(uri: string) {
@@ -20,7 +22,6 @@ export default async function handler(req: NextApiRequest , res: NextApiResponse
   const db = await connectToDatabase(uri)
 
   const collection = db.collection('subscribers')
-
 
   const duplicateEmail = await collection.findOne({ email });
 
@@ -53,14 +54,36 @@ transporter.sendMail({
   html: `<style>
   
   h1, p {
-      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 
+      'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 
+      'Helvetica Neue', sans-serif;
       font-weight: 500;
   }
 
 </style>
 <body>
-  <h3>é um prazer ter você aqui, ${nome}</h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tenetur adipisci quis quod, architecto quasi itaque vero dolore similique eius corporis, pariatur perferendis quas eaque cupiditate repellendus consequatur hic voluptatibus?</p>
+  <h3>é um prazer ter você aqui, no meu cantinho.</h3>
+
+  <p>Sempre que um texto novo for publicado no @atempestadequesou, 
+  irei mandar para você, por aqui. sem spam, e sem links chatos.</p>
+
+  <p>geralmente publico uma vez a cada duas semanas. mas outras, dois textos 
+  por semana. depende do ritmo dos meus sentimentos, dos pensamentos(que são muitos), 
+  e da disponibilidade. </p>
+
+  <p>ah, eu queria poder compartilhar mais do que meus textos por aqui. como livros 
+  que me inspiram, séries que despertam coisas muito boas e qualquer outra coisa que 
+  caiba nos assuntos dos meus textos.</p>
+
+  <p>tente marcar esse email como confiável, assim nunca chegará na caixa de spam. 
+  no mais, obrigado por ler.</p>
+
+  <br/>
+  <p>atempestadequesou</p>
+
+  <br/>
+
+  <a href="https://atempestadequesou.com/delete-email">descadastrar seu email</a>
 </body> `
 
 })
