1bd2f3ab9d84f3915d8c0c11f9008e91cf730a2c
jose-guilherme93 <jose-guilherme93@hotmail.com>
2023-04-24 21:15:08 -0300

    refactor: changing function name

--- pages/delete-email.tsx
+++ pages/delete-email.tsx
@@ -1,29 +1,29 @@
-export default function Hello() {
+export default function DeleteEmail() {
 
     const handleDeleteEmail = () => {
         alert('email apagado da base de dados. uma pena.')
     }
     return (
         <div className="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-l from-yellow-300 to-blue-600 ">
-            <p className="text-zinc-600 font-serif">é uma pena ver você partir.</p>
-            <h1 className="text-3xl text-white font-serif">descadastrar email</h1>
+            <p className="font-serif text-zinc-600">é uma pena ver você partir.</p>
+            <h1 className="font-serif text-3xl text-white">descadastrar email</h1>
 
             <label 
                 htmlFor="inputName" 
-                className="text-white pb-3 font-serif"
+                className="pb-3 font-serif text-white"
                 >
                 digite seu email
                 </label>
             <input 
                 id="inputName"
                 type="email" 
-                className="focus border-2 w-4/5 md:w-80 h-10 border-transparent focus:border-yellow-400 hover:border-yellow-400 rounded-lg mx-4 text-zinc-500 shadow-sm  shadow-zinc-500" 
+                className="w-4/5 h-10 mx-4 border-2 border-transparent rounded-lg shadow-sm focus md:w-80 focus:border-yellow-400 hover:border-yellow-400 text-zinc-500 shadow-zinc-500" 
             />
 
             <button 
                 onClick={handleDeleteEmail} 
                 type="submit" 
-                className="mt-4 shadow-sm drop-shadow-sm shadow-zinc-400 disabled:opacity-40 font-serif text-white text-lg border-solid border-2 rounded-lg h-10 w-48 hover:bg-yellow-400"
+                className="w-48 h-10 mt-4 font-serif text-lg text-white border-2 border-solid rounded-lg shadow-sm drop-shadow-sm shadow-zinc-400 disabled:opacity-40 hover:bg-yellow-400"
                 >
                 enviar
                 </button>
