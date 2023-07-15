7b716589690b5a18c5d82b9419b3e9c3f570be55
jose-guilherme93 <jose-guilherme93@hotmail.com>
2023-07-14 21:26:16 -0300

    chore: :construction: adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   )
 }
