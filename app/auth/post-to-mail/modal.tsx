fabeaaa377f4368df18fb40cd0590efaf9f6c4d9
jose-guilherme93@ <jose-guilherme93@hotmail.com>
2024-01-30 10:05:19 -0300

    style: :lipstick: updating modal

--- app/auth/post-to-mail/modal.tsx
+++ app/auth/post-to-mail/modal.tsx
@@ -5,10 +5,16 @@ interface ModalProps {
     wordCount: number
     closeModal: () => void
     isDisabled: boolean
+    inputTextArea: string
 }
-   export default function Modal({ isDisabled,  confirmModal, closeModal, wordCount }:ModalProps) {
+   export default function Modal({
+      isDisabled,
+      confirmModal, 
+      closeModal, 
+      inputTextArea, 
+      wordCount 
+    } :ModalProps) {
     
-   
     const handleModal = () => {
         !confirmModal
         !isDisabled
@@ -17,20 +23,22 @@ interface ModalProps {
     return (
        
               <div className="bg-zinc-200">
-                 <dialog className="modal" onClose={() => closeModal()} open={confirmModal}>
+                 <dialog className="font-medium modal" onClose={() => closeModal()} open={confirmModal}>
                   <div className="modal-box">
                     
-                  <form method="dialog">
+                  <form className="bg-zinc-200" method="dialog">
                     <button onClick={handleModal} className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</button>
                   </form>
                   
                     <h3 className="text-lg font-bold">Hello!</h3>
                     <p>palavras: {wordCount}</p>
-                    <p className="py-4">certeza que deseja enviar?</p>
-                    <button className="btn btn-primary" disabled={isDisabled} type="submit">enviar</button>
-                    {isDisabled ? " checando o texto..." : " tudo certo!"}
-                  </div>
+                    <p>caracteres: {inputTextArea.length}</p>
+                    {isDisabled ? " checando o texto..." : inputTextArea.length < 75 ? ' menos de 75 caracteres, enviar mesmo assim?' : ' tudo certo!'}
+                    <div className="mt-2">
 
+                    <button className="btn btn-primary w-36" disabled={isDisabled} type="submit">enviar</button>
+                    </div>
+                  </div>
                 </dialog>
               </div>
        
